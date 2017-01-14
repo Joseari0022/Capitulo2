80a3c972d446ebd048eddcb7c145ebc487c59708
Jose Ariel Paredes Gonzalez <jose_paredes2@ucne.edu.do>
2017-01-14 15:16:48 +0100

    Add project files.

---
+++ Calcular_Perimetro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcular_Perimetro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
